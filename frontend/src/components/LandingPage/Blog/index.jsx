8ee0c2c3a7087016507a3ad0f923c4fa18116639
Feruz <140828663+litakk@users.noreply.github.com>
2024-09-16 17:31:45 +0500

    fix.

--- frontend/src/components/LandingPage/Blog/index.jsx
+++ frontend/src/components/LandingPage/Blog/index.jsx
@@ -13,35 +13,35 @@ import BlogItem from "./BlogItem"
 
 function Blog(props) {
     const imgs = [Post1, Post2, Post3]
-        return (
-            <main className="blog-page-wrapper">
-                <Heading title="Blog" size={1.5} />
+    return (
+        <main className="blog-page-wrapper">
+            <Heading title="Blog" size={1.5} />
 
-                <div className="blog-page-content">
-
-
-                    {
-                        BlogJSON.map((blog_item, index) => {
-                            return (
-                                <BlogItem
-                                    key={index}
-                                    img={getImage(imgs, index)}
-                                    id={blog_item.id}
-                                    subtitle={blog_item.subtitle}
-                                    title={blog_item.title}
-                                    author={blog_item.author}
-                                    date={blog_item.date}
-                                    desc1={blog_item.desc1}
-                                />
-                            )
-                        })
-                    }
+            <div className="blog-page-content">
 
 
-                </div>
+                {
+                    BlogJSON.map((blog_item, index) => {
+                        return (
+                            <BlogItem
+                                key={index}
+                                img={getImage(imgs, index)}
+                                id={blog_item.id}
+                                subtitle={blog_item.subtitle}
+                                title={blog_item.title}
+                                author={blog_item.author}
+                                date={blog_item.date}
+                                desc1={blog_item.desc1}
+                            />
+                        )
+                    })
+                }
 
-            </main>
-        );
-    }
+
+            </div>
+
+        </main>
+    );
+}
 
 export default Blog;
